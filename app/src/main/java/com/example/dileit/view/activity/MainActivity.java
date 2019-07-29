a3b7328459a73d277508d045e31147bfa9479cad
yasinhajilou <yasinhaji78@yahoo.com>
2019-07-29 22:51:43 +0430

    add: Home Fragment is show in MainActivity.

--- app/src/main/java/com/example/dileit/view/activity/MainActivity.java
+++ app/src/main/java/com/example/dileit/view/activity/MainActivity.java
@@ -7,14 +7,23 @@ import android.os.Bundle;
 import android.widget.EditText;
 
 import com.example.dileit.R;
+import com.example.dileit.view.fragment.HomeFragment;
 
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setUpHomeFragment();
+    }
+
+    private void setUpHomeFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container_main_activity, new HomeFragment()
+                        , HomeFragment.class.getSimpleName())
+                .commit();
     }
 }

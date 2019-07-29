0937437d0211e45512728dbf76f8c0fa1f9e0978
yasinhajilou <yasinhaji78@yahoo.com>
2019-07-29 22:36:43 +0430

    add:Word Search Fragment added.

---
+++ app/src/main/java/com/example/dileit/view/fragment/WordSearchFragment.java
@@ -0,0 +1,27 @@
+package com.example.dileit.view.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dileit.R;
+
+
+public class WordSearchFragment extends Fragment {
+    public WordSearchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_word_search, container, false);
+    }
+}

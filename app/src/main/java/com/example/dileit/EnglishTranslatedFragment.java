775ad3cf768a871f07020bd948b5851103a27348
yasinhajilou <yasinhaji78@yahoo.com>
2019-11-29 11:39:18 +0330

    add : add 3x fragment

---
+++ app/src/main/java/com/example/dileit/EnglishTranslatedFragment.java
@@ -0,0 +1,30 @@
+package com.example.dileit;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dileit.R;
+
+
+public class EnglishTranslatedFragment extends Fragment {
+
+
+    public EnglishTranslatedFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_english_translated, container, false);
+    }
+
+}

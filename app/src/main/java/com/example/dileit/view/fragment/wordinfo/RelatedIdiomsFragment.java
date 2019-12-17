b6a1429f9f4d113006ef823ab4e2245eed1cb01a
yasinhajilou <yasinhaji78@yahoo.com>
2019-12-17 13:17:58 +0330

    add : idioms view added to databinding

--- app/src/main/java/com/example/dileit/view/fragment/wordinfo/RelatedIdiomsFragment.java
+++ app/src/main/java/com/example/dileit/view/fragment/wordinfo/RelatedIdiomsFragment.java
@@ -3,30 +3,45 @@ package com.example.dileit.view.fragment.wordinfo;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.dileit.R;
+import com.example.dileit.databinding.FragmentRelatedIdiomsBinding;
+import com.example.dileit.viewmodel.SharedViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class RelatedIdiomsFragment extends Fragment {
 
-
-    public RelatedIdiomsFragment() {
-        // Required empty public constructor
+    SharedViewModel mSharedViewModel;
+    FragmentRelatedIdiomsBinding mBinding;
+    RecyclerView mRecyclerView;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSharedViewModel = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_related_idioms, container, false);
+        mBinding = DataBindingUtil.inflate(inflater , R.layout.fragment_related_idioms , container, false);
+        return mBinding.getRoot();
     }
 
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 }

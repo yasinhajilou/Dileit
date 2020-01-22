0d22c1984109bec63472df81cf411cec1d94563b
yasinhajilou <yasinhaji78@yahoo.com>
2020-01-22 16:34:42 +0330

    added : json pojo for res add

--- app/src/main/java/com/example/dileit/model/ResponseObject.java
+++ app/src/main/java/com/example/dileit/model/ResponseObject.java
@@ -7,4 +7,27 @@ public class ResponseObject {
     @SerializedName("response")
     private ResponseStatus mStatus;
 
+    @SerializedName("meta")
+    private ResponseMeta mMeta;
+
+    @SerializedName("data")
+    private ResponseData mData;
+
+    public ResponseObject(ResponseStatus status, ResponseMeta meta, ResponseData data) {
+        mStatus = status;
+        mMeta = meta;
+        mData = data;
+    }
+
+    public ResponseStatus getStatus() {
+        return mStatus;
+    }
+
+    public ResponseMeta getMeta() {
+        return mMeta;
+    }
+
+    public ResponseData getData() {
+        return mData;
+    }
 }

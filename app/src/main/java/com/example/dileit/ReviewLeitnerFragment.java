4532e969d0fbcab3855d52e5872302ecba2508df
yasinhajilou <yasinhaji78@yahoo.com>
2020-02-03 12:37:26 +0330

    added : Review leitner xml added

---
+++ app/src/main/java/com/example/dileit/ReviewLeitnerFragment.java
@@ -0,0 +1,54 @@
+package com.example.dileit;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dileit.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ReviewLeitnerFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class ReviewLeitnerFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_review_leitner, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}

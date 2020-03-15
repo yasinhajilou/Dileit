a0613769fe2fed229d09fac7205cab38d540e8f6
yasinhajilou <yasinhaji78@yahoo.com>
2020-03-15 22:48:23 +0330

    added : second translation fragment dialog

---
+++ app/src/main/java/com/example/dileit/view/fragment/wordinfo/leitnersetup/SecondTranslationDialogFragment.java
@@ -0,0 +1,22 @@
+package com.example.dileit.view.fragment.wordinfo.leitnersetup;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dileit.R;
+
+
+public class SecondTranslationDialogFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second_translation_dialog, container, false);
+    }
+}

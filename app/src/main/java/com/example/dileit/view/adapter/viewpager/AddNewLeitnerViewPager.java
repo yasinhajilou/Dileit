f4295505fe54acc082d38742142b0729e4b3fc7e
yasinhajilou <yasinhaji78@yahoo.com>
2020-03-15 23:04:50 +0330

    added : fragments added

--- app/src/main/java/com/example/dileit/view/adapter/viewpager/AddNewLeitnerViewPager.java
+++ app/src/main/java/com/example/dileit/view/adapter/viewpager/AddNewLeitnerViewPager.java
@@ -1,11 +1,16 @@
 package com.example.dileit.view.adapter.viewpager;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.example.dileit.view.fragment.wordinfo.leitnersetup.SecondTranslationDialogFragment;
+import com.example.dileit.view.fragment.wordinfo.leitnersetup.TranslationDialogFragment;
+
 public class AddNewLeitnerViewPager extends FragmentPagerAdapter {
+    String[] titles = new String[]{"Translation" , "EnglishTranslation"};
 
     public AddNewLeitnerViewPager(@NonNull FragmentManager fm) {
         super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
@@ -14,15 +19,28 @@ public class AddNewLeitnerViewPager extends FragmentPagerAdapter {
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        switch (position){
+        Fragment fragment;
+        switch (position) {
             case 0:
-
+                fragment =  new TranslationDialogFragment();
+                break;
+            case 1:
+                fragment =  new SecondTranslationDialogFragment();
+                break;
+            default:
+                fragment = new TranslationDialogFragment();
         }
-        return null;
+        return fragment;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
     }
 }
--- app/src/main/java/com/example/dileit/view/fragment/wordinfo/leitnersetup/SecondTranslationDialogFragment.java
+++ app/src/main/java/com/example/dileit/view/fragment/wordinfo/leitnersetup/SecondTranslationDialogFragment.java
@@ -2,21 +2,45 @@ package com.example.dileit.view.fragment.wordinfo.leitnersetup;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.dileit.R;
+import com.example.dileit.viewmodel.SharedViewModel;
 
 
 public class SecondTranslationDialogFragment extends Fragment {
+    private SharedViewModel mSharedViewModel;
+    private EditText mEditText;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSharedViewModel = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second_translation_dialog, container, false);
+        View view = inflater.inflate(R.layout.fragment_second_translation_dialog, container, false);
+        mEditText = view.findViewById(R.id.edt_second_translation_dialog);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mSharedViewModel.getLeitnerItemData().observe(getViewLifecycleOwner(),strings -> {
+            mEditText.setText(strings[1]);
+        });
     }
 }

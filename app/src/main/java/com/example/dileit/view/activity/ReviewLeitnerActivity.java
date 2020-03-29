fdc8e569c0cb61fb925984b8672855f8f94ad439
yasinhajilou <yasinhaji78@yahoo.com>
2020-03-29 21:32:49 +0430

    added : leitner activity added

---
+++ app/src/main/java/com/example/dileit/view/activity/ReviewLeitnerActivity.java
@@ -0,0 +1,16 @@
+package com.example.dileit.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dileit.R;
+
+public class ReviewLeitnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_leitner);
+    }
+}

d17718ddb7838645296b2cf6dd5df53c42f572d6
yasinhajilou <yasinhaji78@yahoo.com>
2020-03-29 21:32:23 +0430

    added : leitner activity added

--- app/src/main/java/com/example/dileit/view/adapter/viewpager/LeitnerReviewViewPagerAdapter.java
+++ app/src/main/java/com/example/dileit/view/adapter/viewpager/LeitnerReviewViewPagerAdapter.java
@@ -2,31 +2,29 @@ package com.example.dileit.view.adapter.viewpager;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class LeitnerReviewViewPagerAdapter extends FragmentStateAdapter {
+public class LeitnerReviewViewPagerAdapter extends FragmentStatePagerAdapter {
 
     List<Fragment> mFragments = new ArrayList<>();
-    public LeitnerReviewViewPagerAdapter(@NonNull Fragment fragment) {
-        super(fragment);
+
+    public LeitnerReviewViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
     }
 
     @NonNull
     @Override
-    public Fragment createFragment(int position) {
-        return mFragments.get(position);
+    public Fragment getItem(int position) {
+        return null;
     }
 
     @Override
-    public int getItemCount() {
-        return mFragments.size();
-    }
-
-    public void addPage(Fragment fragment){
-        mFragments.add(fragment);
-        notifyDataSetChanged();
+    public int getCount() {
+        return 0;
     }
 }

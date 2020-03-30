6259c753796ebf22ea6d319d2ade03c63c42d4ed
yasinhajilou <yasinhaji78@yahoo.com>
2020-03-30 16:40:59 +0430

    removed : two frgment removed

--- app/src/main/java/com/example/dileit/view/activity/ReviewLeitnerActivity.java
+++ app/src/main/java/com/example/dileit/view/activity/ReviewLeitnerActivity.java
@@ -5,12 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.dileit.R;
+import com.example.dileit.databinding.ActivityReviewLeitnerBinding;
+import com.example.dileit.view.adapter.viewpager.LeitnerReviewViewPagerAdapter;
 
 public class ReviewLeitnerActivity extends AppCompatActivity {
 
+    LeitnerReviewViewPagerAdapter mAdapter;
+    ActivityReviewLeitnerBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_leitner);
+
+        mBinding = ActivityReviewLeitnerBinding.inflate(getLayoutInflater());
+        mAdapter = new LeitnerReviewViewPagerAdapter(getSupportFragmentManager());
+
     }
 }

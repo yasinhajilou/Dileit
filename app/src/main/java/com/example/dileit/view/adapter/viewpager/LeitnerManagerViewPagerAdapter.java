749b3ce5b7e007965b076ca1f948ba03ca4db907
yasinhajilou <yasinhaji78@yahoo.com>
2020-05-11 13:30:45 +0430

    fided : tablayout bug fixed

--- app/src/main/java/com/example/dileit/view/adapter/viewpager/LeitnerManagerViewPagerAdapter.java
+++ app/src/main/java/com/example/dileit/view/adapter/viewpager/LeitnerManagerViewPagerAdapter.java
@@ -1,18 +1,22 @@
 package com.example.dileit.view.adapter.viewpager;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.example.dileit.R;
 import com.example.dileit.view.fragment.leitnermanager.LearnedWordsManagerFragment;
 import com.example.dileit.view.fragment.leitnermanager.NewWordsManagerFragment;
 import com.example.dileit.view.fragment.leitnermanager.ReviewWordsManagerFragment;
 
 public class LeitnerManagerViewPagerAdapter extends FragmentPagerAdapter {
 
+
     public LeitnerManagerViewPagerAdapter(@NonNull FragmentManager fm) {
         super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
     }
 
     @NonNull
@@ -33,5 +37,20 @@ public class LeitnerManagerViewPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "New";
+            case 1:
+                return "Review";
+            case 2:
+                return  "Learned";
+            default:
+                return  "New";
+        }
     }
 }

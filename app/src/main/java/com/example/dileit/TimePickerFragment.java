44de837901ad3d83ff62de14028f450887786168
yasin hajiloo <41325533+yasinhajiloo@users.noreply.github.com>
2020-06-30 13:57:48 +0430

    added : TimePickerFragment added

---
+++ app/src/main/java/com/example/dileit/TimePickerFragment.java
@@ -0,0 +1,35 @@
+package com.example.dileit;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        final Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int min = calendar.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getContext(), this, hour, min, true);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+
+    }
+}
--- app/src/main/java/com/example/dileit/view/activity/SettingActivity.java
+++ app/src/main/java/com/example/dileit/view/activity/SettingActivity.java
@@ -3,17 +3,34 @@ package com.example.dileit.view.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.dileit.R;
+import com.example.dileit.TimePickerFragment;
 import com.example.dileit.databinding.ActivitySettingBinding;
 
 public class SettingActivity extends AppCompatActivity {
 
     private ActivitySettingBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = ActivitySettingBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
+
+        setSupportActionBar(mBinding.toolbarSetting);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        mBinding.linearLayoutTimeEditor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerFragment timePickerFragment = new TimePickerFragment();
+                timePickerFragment.show(getSupportFragmentManager() , "DIALOG_TIME_PICKER");
+            }
+        });
     }
 }

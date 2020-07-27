6e9bad170698a33be013a49282f3ce86c31451ef
yasin hajiloo <41325533+yasinhajiloo@users.noreply.github.com>
2020-07-27 16:55:03 +0430

    removed : closing db removed temporarily

--- app/src/main/java/com/example/dileit/model/database/persiandb/PersianDatabaseAccess.java
+++ app/src/main/java/com/example/dileit/model/database/persiandb/PersianDatabaseAccess.java
@@ -33,8 +33,8 @@ public class PersianDatabaseAccess {
     }
 
     public void closeDatabase() {
-        if (this.mDatabase != null)
-            mHelper.close();
+//        if (this.mDatabase != null)
+//            mHelper.close();
     }
 
     @Override

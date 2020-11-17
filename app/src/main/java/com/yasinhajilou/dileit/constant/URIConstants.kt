5f2b8c2fe374eb24698dc09c6c53215890206073
yasinhajiloo <yasinhaji78@yahoo.com>
2020-11-17 15:46:23 +0330

    modified : telegram link

--- app/src/main/java/com/yasinhajilou/dileit/constant/URIConstants.kt
+++ app/src/main/java/com/yasinhajilou/dileit/constant/URIConstants.kt
@@ -4,6 +4,6 @@ object URIConstants {
     const val githubUrl = "https://github.com/yasinhajiloo/Dileit"
     const val instagramURL = "http://instagram.com/_u/dileit.ir"
     const val instagramPackageURI = "com.instagram.android"
-    const val telegramURL = "https://t.me/dileit"
+    const val telegramURL = "https://t.me/YasCodes"
     const val email = "dileit2019@gmail.com"
 }
